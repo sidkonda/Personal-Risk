10dd948beaac5aa403183247235bb1cfe99206d2
Siddhartha Konda <sidkonda@github.com>
2019-09-13 10:20:42 -0700

    Update run.rb

--- First-Responder/run.rb
+++ First-Responder/run.rb
@@ -7,11 +7,11 @@ ISSUE_TEMPLATE = File.read(File.join(File.dirname(__FILE__), "template.md"))
 client = Octokit::Client.new(:access_token => ENV["GITHUB_TOKEN"])
 client.auto_paginate = true
 
-open_issues = client.list_issues(NWO, { :labels => "weekly-meeting" })
+open_issues = client.list_issues(NWO, { :labels => "first-responder" })
 
 open_issues.each do |issue|
   client.close_issue(NWO, issue.number)
 end
 
-client.create_issue(NWO, "Weekly Meeting" , ISSUE_TEMPLATE, { :labels => "weekly-meeting" })
+client.create_issue(NWO, "First Responder" , ISSUE_TEMPLATE, { :labels => "first-responder" })
 
