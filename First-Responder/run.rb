2748c2aa0fe8f27e869d06c097d9d0992953b855
Siddhartha Konda <sidkonda@github.com>
2019-09-27 15:56:49 -0700

    Change Issue Creation to Comment Addition

--- First-Responder/run.rb
+++ First-Responder/run.rb
@@ -7,11 +7,5 @@ ISSUE_TEMPLATE = File.read(File.join(File.dirname(__FILE__), "template.md"))
 client = Octokit::Client.new(:access_token => ENV["GITHUB_TOKEN"])
 client.auto_paginate = true
 
-open_issues = client.list_issues(NWO, { :labels => "first-responder" })
-
-open_issues.each do |issue|
-  client.close_issue(NWO, issue.number)
-end
-
-client.create_issue(NWO, "First Responder" , ISSUE_TEMPLATE, { :labels => "first-responder" })
+client.add_comment(NWO, 32 , ISSUE_TEMPLATE, {})
 
